5984465928f25930307d58e8cc2bd4e1b5940d3b
javierMontufar <arbeit.mount@gmail.com>
2026-09-26 13:46:57 -0600

    Add: monotonic-array - submission-5

---
+++ Data Structures & Algorithms/monotonic-array/submission-5.py
@@ -0,0 +1,35 @@
+import heapq
+
+class Solution:
+    def isMonotonic(self, nums: List[int]) -> bool:
+        # montone_incr = sorted(nums)
+        # monotone_dec = sorted(nums, reverse=True)
+
+        # return (nums == montone_incr) or (nums == monotone_dec)
+
+        numbers = nums[:]
+        numbers_max = nums[:]
+        numbers_max = [-number for number in numbers_max]
+        # min heap numbers
+        heapq.heapify(numbers)
+        value = True
+
+        for index in range(len(nums)):
+            min_el = heapq.heappop(numbers)
+            if nums[index] != min_el:
+                value = False
+                break
+        
+
+        numbers_max = nums[:]
+        numbers_max = [-number for number in numbers_max]
+        heapq.heapify(numbers_max)
+        value2 = True
+
+        for index in range(len(nums)):
+            max_el = -heapq.heappop(numbers_max)
+            if nums[index] != max_el:
+                value2 = False
+                break
+        
+        return value or value2
